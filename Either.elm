a87b46e45c894deecadbee222f3aa9d7a66f3d45
Izaak Meckler <ihmeckler@gmail.com>
2015-11-23 19:32:30 -0500

    updating for elm bump

--- Either.elm
+++ Either.elm
@@ -12,35 +12,47 @@ module Either where
 @docs elim, isLeft, isRight
 -}
 
+{-| For when you want a function that needs to take/return values of
+    two different types and you don't want to define a whole type. -}
 type Either a b
   = Left a
   | Right b
 
+{-| There's only one possible implementation of this function! Try to guess
+    what it is. -}
 mapRight : (b -> b') -> Either a b -> Either a b'
 mapRight f ex = case ex of
   Left a  -> Left a
   Right b -> Right (f b)
 
+{-| There's only one possible implementation of this function! Try to guess
+    what it is. -}
 mapLeft : (a -> a') -> Either a b -> Either a' b
 mapLeft f ex = case ex of
   Left a  -> Left (f a)
   Right b -> Right b
 
+{-| There's only one possible implementation of this function! Try to guess
+    what it is. -}
 mapBoth : (a -> a') -> (b -> b') -> Either a b -> Either a' b'
 mapBoth f g ex = case ex of
   Left a  -> Left (f a)
   Right b -> Right (g b)
 
+{-| There's only one possible implementation of this function! Try to guess
+    what it is. -}
 elim : (a -> c) -> (b -> c) -> Either a b -> c
 elim f g ex = case ex of
   Left a  -> f a
   Right b -> g b
 
+{-| -}
 isLeft : Either a b -> Bool
 isLeft ex = case ex of
   Left _ -> True
   _      -> False
   
+{-| -}
 isRight : Either a b -> Bool
 isRight ex = case ex of
   Right _ -> True
